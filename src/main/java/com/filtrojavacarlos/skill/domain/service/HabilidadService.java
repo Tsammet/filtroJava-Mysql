279ec835e1376d04abb0b3fa6200dd3e435a37ca
papasote <xxx>
2024-07-26 17:52:41 -0500

    habilidades

---
+++ src/main/java/com/filtrojavacarlos/skill/domain/service/HabilidadService.java
@@ -0,0 +1,9 @@
+package com.filtrojavacarlos.skill.domain.service;
+
+import com.filtrojavacarlos.skill.domain.entity.Habilidad;
+
+public interface HabilidadService {
+
+    void createHabilidad(Habilidad habilidad);
+
+}
